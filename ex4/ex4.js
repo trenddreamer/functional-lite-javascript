84dc3d991518abf79955cba90aa9a0b795ec3c81
Barry <trenddreamer@gmail.com>
2019-10-28 15:32:30 +0200

    ex4

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,1 +1,53 @@
-// code here! :)
+// thunk
+function foo(x) {
+  return function() {
+    return x;
+  };
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+//add2(foo(10), foo(42)); //52
+
+//with loop
+function addn(arr) {
+  var sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = add2(foo(arr[i]), foo(sum));
+  }
+  return sum;
+}
+
+//recursion with add
+function addn(vals) {
+  if (vals.length > 2) {
+    return addn([add(vals[0], vals[1])].concat(vals.slice(2)));
+  }
+  return add(vals[0], vals[1]);
+}
+//recursion with add2
+function addn(arr) {
+  if (arr.length <= 2) {
+    return add2(foo(arr[0]), foo(arr[1]));
+  }
+  return add2(foo(arr[0]), foo(addn(arr.slice(1))));
+}
+//map and reduce with add2
+function addn(arr) {
+  return arr
+    .slice(1)
+    .map(foo)
+    .reduce(function(prev, cur) {
+      return function() {
+        return add2(prev, cur);
+      };
+    }, foo(arr[0]))();
+}
+
+addnr([10, 42, 56, 73]);
---
+++ ex4/ex4s.js
@@ -0,0 +1,29 @@
+function foo(v) {
+  return function() {
+    return v;
+  };
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+function add2(fn1, fn2) {
+  return add(fn1(), fn2());
+}
+
+function addn(arr) {
+  return arr
+    .slice(1)
+    .map(foo)
+    .reduce(function(prev, cur) {
+      return function() {
+        return add2(prev, cur);
+      };
+    }, foo(arr[0]))();
+}
+
+var valsx = [foo(10), foo(100), foo(30), foo(100), foo(42), foo(10), foo(15)];
+var vals = [10, 100, 30, 100, 42, 10, 15];
+
+console.log(addn(vals));
